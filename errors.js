6c53217f111ce74e911aade0cbbf2a700efb1657
Marc Tiburcio <44109710+marcnt@users.noreply.github.com>
2020-05-01 23:07:36 +0800

    Add error codes for AJAX endpoints

---
+++ errors.js
@@ -0,0 +1,45 @@
+
+// ERROR CODE DEFINITIONS
+
+const errors = {
+  GENERIC_ERROR: "GENERIC_ERROR",
+
+  // Thrown when an endpoint requires a field but is not provided in the request.
+  MISSING_FIELD: "MISSING_FIELD",
+  // Thrown when a value is provided but is in an invalid type or format.
+  INVALID_VALUE: "INVALID_VALUE",
+
+  // Thrown when a protected endpoint receives a request without a valid session.
+  UNAUTHORIZED: "UNAUTHORIZED",
+
+  // Thrown when the resource to be operated upon or searched is not present.
+  NOT_FOUND: "NOT_FOUND",
+
+  create: function(type, message, details) {
+    if (details)  return { type: type || this.GENERIC_ERROR, message: message || "No message provided", details: details };
+    else          return { type: type || this.GENERIC_ERROR, message: message || "No message provided" };
+  },
+  generic: function(message, details) {
+    return this.create(this.GENERIC_ERROR, message, details);
+  },
+  missingField: function(field, message, details) {
+    if (!details) details = {};
+    details.field = field;
+    
+    return this.create(this.MISSING_FIELD, message, details);
+  },
+  invalidValue: function(field, message, details) {
+    if (!details) details = {};
+    details.field = field;
+    
+    return this.create(this.INVALID_VALUE, message, details);
+  },
+  unauthorized: function(message, details) {
+    return this.create(this.UNAUTHORIZED, message || "Not logged in.", details);
+  },
+  notFound: function(message, details) {
+    return this.create(this.NOT_FOUND, message || "Resource not found.", details);
+  }
+};
+
+module.exports = errors;
