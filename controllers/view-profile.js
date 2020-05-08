89573b4620dd752541970263738ac64d3e594fe5
Marc Tiburcio <44109710+marcnt@users.noreply.github.com>
2020-05-08 18:46:09 +0800

    Fix profile view and registration

--- controllers/view-profile.js
+++ controllers/view-profile.js
@@ -73,26 +73,67 @@ const viewProfileController = {
 
   getOwnProfile: function(req, res){
     if (req.session && req.session.loggedIn) {
-      Profile.findOne({ username: req.session.user.username }).exec(function(err, user) {
+      var profileToView = req.session.user.username;
+      Profile.findOne({ username: profileToView }).exec(function(err, user) {
         if (err) {
           return next(err);
         } else if (!user) {
           res.redirect('/404');
         } else {
-          res.render('profile', {
-            layout: 'with-nav',
-            registered: true,
-            user: req.session.user,
-            self: true,
-            profile: {
-              display: (user.firstname && user.lastname) ? user.firstname + ' ' + user.lastname : user.username,
-              bio: user.bio,
-              join_date: user.join_date,
-              picture_link: user.picture_link
-            },
-            class: 'bg-cstm-yellow-lightest',
-            title: 'My Profile',
-            recipe_title: 'My Recipes'
+          // load recipes made by this user
+          Recipe.find({ author: user._id }).exec(function(err, recipes) {
+            if (err) {
+              return next(err);
+            } else {
+              var params = {
+                layout: 'with-nav',
+                registered: false,
+                self: true,
+                profile: {
+                  display: user.display_name,
+                  bio: user.bio,
+                  join_date: user.join_date,
+                  picture_link: user.picture_link
+                },
+                class: 'bg-cstm-yellow-lightest',
+                title: 'My Profile',
+                uploaded_recipes: {
+                  section_name: 'uploaded-recipes',
+                  values: recipes.map(d => d.toObject())  
+                }
+              };
+              params.recipe_title = 'My Recipes';
+        
+              if (req.session && req.session.loggedIn) {
+                params.registered = true;
+                params.is_admin = req.session.isAdmin;
+                params.user = req.session.user;
+              }
+
+              // get all recipes this user has liked
+              Like.find({ sender: user._id }).populate('recipe').exec(function(err, likes) {
+                if (err) {
+                  return next(err);
+                } else {
+                  var liked_recipes = likes.map(d => d.recipe.toObject());
+                  params.liked_recipes = {
+                    section_name: 'liked-recipes',
+                    values: liked_recipes
+                  };
+
+                  Comment.find({ author: user._id }).populate('recipe').exec(function(err, comments) {
+                    var allComments = comments.map(d => d.toObject());
+                    params.comments = allComments;
+
+                    if (err) {
+                      return next(err);
+                    } else {
+                      res.render('profile', params);
+                    }
+                  });
+                }
+              });
+            }
           });
         }
       });
